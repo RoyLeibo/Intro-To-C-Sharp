278b2599a1b6379497ac7a6cd6919f68555c0de2
Roy Leibovitz <royleibo212@gmail.com>
2019-03-08 21:51:04 +0200

    First Upload

---
+++ Exercise_1/Exercise_1/FunctionContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class FunctionContainer
+    {
+        Dictionary<String, Func<double, double>> funcList;
+
+        public FunctionContainer()
+        {
+            funcList = new Dictionary<String, Func<double, double>>() ;
+        }
+
+        public void setNewFunc(String key, Func<double, double> value)
+        {
+            this.funcList[key] = value;
+        }
+
+        public Func<double, double> getFunc(String key)
+        {
+            return this.funcList[key];
+        }
+    }
+}
---
+++ Exercise_1/Exercise_1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FunctionContainer fc = new FunctionContainer();
+            fc.setNewFunc("Double", Double);
+            Func<double, double> thisFunc = fc.getFunc("Double");
+            Console.WriteLine(thisFunc(2));
+        }
+
+        static double Double(double a)
+        {
+            return a * 2;
+        }
+    }
+}
